7120b6cbe4259221c093d7da5e4d9b5c3a402bb2
Oscar Gomez <oscargomezgomezluna@gmail.com>
2024-09-06 18:47:32 -0500

    POS4 create table post

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { Post } from "./Post";
 
 @Entity()
 export class User {
@@ -22,4 +23,7 @@ export class User {
 
   @Column()
   zipcode: string;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 }
