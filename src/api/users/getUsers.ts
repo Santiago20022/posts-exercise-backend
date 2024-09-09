86f1fa5ee5c56550dbc662e9aabfe7d37d68eda7
Santiago <santiago.garcia.2007.22@gmail.com>
2024-09-09 18:47:41 -0500

    Fix indentation

--- src/api/users/getUsers.ts
+++ src/api/users/getUsers.ts
@@ -3,15 +3,15 @@ import { AppDataSource } from "../../data-source";
 import { User } from "../../entity/User";
 
 export async function getUsers(req: Request, res: Response) {
-    try {
-        const userRepository = AppDataSource.manager.getRepository(User);
-        const users = await userRepository.find();
-        if (!users || users.length === 0) {
-            return  res.status(404).send({ message: "Users not found" }); 
-            }
-        return res.send(users).status(200);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: "Error fetching users" });
-    }
+	try {
+		const userRepository = AppDataSource.manager.getRepository(User);
+		const users = await userRepository.find();
+		if (!users || users.length === 0) {
+			return  res.status(404).send({ message: "Users not found" }); 
+		}
+		return res.send(users).status(200);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send({ message: "Error fetching users" });
+	}
 }
