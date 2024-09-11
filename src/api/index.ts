94a4ea505f931b958cc34c5dbaa3c5bdf49411f2
Oscar Gomez <oscargomezgomezluna@gmail.com>
2024-09-11 16:39:21 -0500

    POS-10 create endpoint get all comment

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { getUsers } from './users/getUsers';
 import { getPosts } from './posts/getPosts';
 import { getTodos } from './todos/getTodos';
+import { getComments } from './comments/getComments';
 
 const router = express.Router();
 
 router.get('/get/users', (req, res) => getUsers(req, res));
 router.get('/get/posts', (req, res) => getPosts(req, res));
 router.get('/get/todos', (req, res) => getTodos(req, res));
+router.get('/get/comments', (req, res) => getComments(req, res));
 
 export default router;
