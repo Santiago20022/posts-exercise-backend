e9061a89dad6bfd6a3328f5d8a39a93d669930cb
Santiago <santiago.garcia.2007.22@gmail.com>
2024-09-11 14:21:00 -0500

    Create api endpoint to get all posts

---
+++ src/api/posts/getPosts.ts
@@ -0,0 +1,22 @@
+import type { Request, Response } from "express";
+import { AppDataSource } from "../../data-source";
+import { Post } from "../../entity/Post";
+
+export async function getPosts(req: Request, res: Response) {
+  try {
+    const postRepository = AppDataSource.manager.getRepository(Post);
+    const posts = await postRepository.find({
+      relations: {
+        user: true,
+      },
+       loadEagerRelations: false,
+    });
+    if (!posts || posts.length === 0) {
+      return res.status(404).send({ message: "Posts not found" });
+    }
+    return res.send(posts).status(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Error fetching posts" });
+  }
+}
