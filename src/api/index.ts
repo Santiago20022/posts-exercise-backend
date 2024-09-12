982ce59a31f4425d4045caba3dc690e6bfccdcda
Santiago <santiago.garcia.2007.22@gmail.com>
2024-09-12 11:13:37 -0500

    Create a api endpoint to create a single user

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { getUsers } from './users/getUsers';
 import { getPosts } from './posts/getPosts';
 import { getTodos } from './todos/getTodos';
+import { createUser } from './users/createUser';
 
 const router = express.Router();
 
 router.get('/get/users', (req, res) => getUsers(req, res));
 router.get('/get/posts', (req, res) => getPosts(req, res));
 router.get('/get/todos', (req, res) => getTodos(req, res));
+router.post("/users", createUser);
 
 export default router;
