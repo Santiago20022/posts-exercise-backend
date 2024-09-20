01ab0004d9063c572d87a9115cc69c718e89260b
Santiago <santiago.garcia.2007.22@gmail.com>
2024-09-20 11:25:00 -0500

    fix Cross-Origin Resource Sharing

--- src/api/index.ts
+++ src/api/index.ts
@@ -16,6 +16,11 @@ import { getCommentsByPostId } from './comments/getAllComments';
 
 import { getTodosByUserId } from './todos/getTodosByUserId';
 const router = express.Router();
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.get('/get/users', (req, res) => getUsers(req, res));
 router.get('/get/posts', (req, res) => getPosts(req, res));
